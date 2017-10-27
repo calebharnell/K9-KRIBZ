1dd278bd7543fe589f32272083180ccdfaa513a2
calebharnell <calebharnell@hotmail.com>
2017-10-27 13:52:35 +1000

    Add to User model and devise forms

---
+++ db/migrate/20171027020239_add_columns_to_user.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :suburb, :string
+    add_column :users, :state, :string
+  end
+end
