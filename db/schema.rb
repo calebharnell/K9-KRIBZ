19c34561e77904e9a6e7983da823f2b95a1b6b30
calebharnell <calebharnell@hotmail.com>
2017-10-28 22:43:51 +1000

    Add dogs scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027045141) do
+ActiveRecord::Schema.define(version: 20171028122358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dogs", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.string "age"
+    t.string "breed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_dogs_on_user_id"
+  end
 
   create_table "listings", force: :cascade do |t|
     t.bigint "user_id"
@@ -47,5 +57,6 @@ ActiveRecord::Schema.define(version: 20171027045141) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "dogs", "users"
   add_foreign_key "listings", "users"
 end
