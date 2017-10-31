0a2a2bd7bb681c36edbd08c804ed93526cd3fefd
calebharnell <calebharnell@hotmail.com>
2017-10-31 15:45:11 +1000

    Ratings and Map updates

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,11 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_many :dogs, through: :users
+
+  def full_street_address
+    "#{suburb}, #{state}"
+  end
+
+  geocoded_by :full_street_address
+  after_validation :geocode
 end
