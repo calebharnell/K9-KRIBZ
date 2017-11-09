2be99aaa063d2a6d5f66f78a473735298b3e722e
calebharnell <calebharnell@hotmail.com>
2017-11-09 13:55:51 +1000

    Order users by distance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.near("#{current_user.suburb} #{current_user.state}", 8_000_000 , order: 'distance')
   end
   def show
     @user = User.find(params[:id])
